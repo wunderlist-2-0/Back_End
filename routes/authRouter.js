7ab897c97e1a68f7228d60f31489e0ddb5085ad7
J A <jarata@gmail.com>
2019-03-18 14:18:54 -0700

    auth.js created, handles token generation and authentication login POST endpoint created

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const database = require('../database/dbConfig');
 const bcrypt = require('bcryptjs');
+// Token generation, cookie
+const { genToken } = require('../database/helpers/auth');
 
 // POST register
 router.post('/register', async (req, res) => {
@@ -23,7 +25,23 @@ router.post('/register', async (req, res) => {
 
 // POST login
 router.post('/login', async (req, res) => {
-
-})
+    let { username, password } = req.body;
+    try {
+        const user = await database('users').where({ username }).first();
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = genToken(user);
+            res.status(200).json({
+                message: `Welcome ${user.username}, you are logged in for 1 hour!`,
+                token
+            })
+        } else {
+            res.status(401).json({
+                error: 'Invalid credentials'
+            })
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
 
 module.exports = router;
