57f3a2309c6d18160bd80534deacbc4f4a2e7b18
J A <jarata@gmail.com>
2019-03-19 10:12:45 -0700

    todoRouter layout

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
 const db = require('../database/dbConfig');
+const { auth } = require('../database/helpers/auth');
+
+// GET todos
+router.get('/todos', auth, async (req, res) => {
+    res.send(`GET todo's auth test!`)
+});
+
+// // GET/:id todos
+// router.get('/todos/:id', auth, async (req, res) => {
+//
+// });
+//
+// // POST todos
+// router.get('/todos', auth, async (res, res) => {
+//
+// });
+//
+// // PUT/:id todos
+// router.put('/todos/:id', auth, async (res, res) => {
+//
+// });
+//
+// // DELETE/:id todos
+// router.delete('/todos/:id', auth, async (req, res) => {
+//
+// });
 
 modules.export = router;
