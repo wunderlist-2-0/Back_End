b3dc0351089756c9b71829cebf6e4e4327637613
J A <jarata@gmail.com>
2019-03-19 20:36:21 -0700

    todoRouter DELETE and POST, need to fix

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -19,38 +19,48 @@ router.get('/list', async (req, res) => {
 // });
 
 // POST todos
-// router.post('/create', async (req, res) => {
-//     const todoT = req.body;
-//     try {
-//         if (!todoT.title) {
-//             res.status(404).json({
-//                 message: "Todo needs a title"
-//             })
-//         }
-//         const todo = await database('todos').insert(todoT);
-//         res.status(201).json({
-//             message: "Todo has been created",
-//             todo
-//         })
-//     } catch (e) {
-//         res.status(500).json(e)
-//     }
-// });
+router.post('/create', async (req, res) => {
+    const todoT = req.body;
+    try {
+        if (!todoT.title) {
+            res.status(404).json({
+                message: "Todo needs a title"
+            })
+        }
+        const todo = await database('todos').insert(todoT);
+        res.status(201).json({
+            message: "Todo has been created",
+            todo
+        })
+    } catch (e) {
+        res.status(500).json(e)
+    }
+});
 
 // // PUT/:id todos
 // router.put('/edit/:id', async (res, res) => {
 //
 // });
 //
+
 // DELETE/:id todos
-// router.delete('/delete/:id', async (req, res) => {
-//     const {id} = req.params;
-//     const count = await database('todos').where({id}).del();
-//     try {
-//         if (count > 0) {
-//             res.status
-//         }
-//     }
-// });
+router.delete('/delete/:id', async (req, res) => {
+    const id = req.body.id;
+    const count = await database('todos').where(id).del();
+    try {
+        if (count > 0) {
+            res.status(200).json({
+                message: "Todo has been deleted"
+            })
+        } else {
+            res.status(404).json({
+                error: "The todo with the specified ID does not exist"
+            })
+        }
+    } catch (e) {
+        console.log(req.params)
+        res.status(500).json(e)
+    }
+});
 
 module.exports = router;
