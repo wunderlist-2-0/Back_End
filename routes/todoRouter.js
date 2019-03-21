8ca532265c8278c3a202b41d91afa2eb3181859f
J A <jarata@gmail.com>
2019-03-20 21:19:57 -0700

    GET/:id still not working cleaned up logged middleware

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -3,20 +3,31 @@ const database = require('../database/dbConfig');
 
 // GET todos
 router.get('/list', async (req, res) => {
+    const todo = await database('todos').where('userId', req.decoded.subject);
     try {
-        console.log(req.decoded.subject)
-        const todo = await database('todos').where('userId', req.decoded.subject);
         res.status(200).json(todo)
     } catch (error) {
-        res.status(500).json({
-            error: "The todo's information could not be retrieved"
-        })
+        res.status(500).json(e)
     }
 });
 
 // GET/:id todos
 router.get('/list/:id', async (req, res) => {
-
+    const id = req.params.id;
+    const userTodos = await database('todos').where('userId', req.decoded.subject);
+    try {
+        if (req.decoded.subject) {
+            const test = await userTodos.where(id);
+            console.log(test)
+            res.status(200).json(test)
+        } else {
+            res.status(404).json({
+                error: "Todo with the specified ID does not exist"
+            })
+        }
+    } catch (e) {
+        res.status(500).json(e)
+    }
 });
 
 // POST todos
